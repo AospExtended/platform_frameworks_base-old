097d8c8ad696ff7505fe9584b081125d676c89e6
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-29 21:43:37 +0100

    Forward Port: Volume button seek on screen off [1/2]
    PS : Fix the short press event type
    
    Adapted for Marshmallow by zephiK
    
    Change-Id: Id55a1240a133b5144502fced862bae040b390662
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    
    Conflicts:
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3481,6 +3481,12 @@ public final class Settings {
        public static final String USB_DATA_AUTO_UNLOCK = "usb_data_auto_unlock";
 
         /**
+         * Volume music controls
+         * @hide
+         */
+        public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+      
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
