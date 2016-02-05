4a989bd45cdc9ac40969a8bb1691563842efb6d3
Raleigh Matlock <raleighman2@gmail.com>
2016-02-04 22:42:49 -0500

    Fix activity manager service

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1472,7 +1472,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
                         if (Settings.System.getInt(mContext.getContentResolver(),
                                 Settings.System.DISABLE_FC_NOTIFICATIONS, 0) != 1) {
-                            Dialog d = new AppErrorDialog(mContext(),
+                            Dialog d = new AppErrorDialog(mContext,
                                 ActivityManagerService.this, res, proc);
                             d.show();
                             proc.crashDialog = d;
