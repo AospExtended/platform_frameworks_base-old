ce4409b672856bfbf57d4d8d393133b63999e1d4
Raleigh Matlock <raleighman2@gmail.com>
2016-04-26 10:55:42 -0400

    Make the battery fully charged notification a toggle (1/2)
    Change-Id: I4346051f57ecd0f21462c2d3caa72a4748fe6238
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3626,6 +3626,12 @@ public final class Settings {
         public static final String RECENTS_SHOW_SEARCH_BAR = "recents_show_search_bar";
 
         /**
+         * Whether to show the battery fully charged notification
+         * @hide
+         */
+        public static final String BATTERY_FULLY_CHARGED_NOTIFICATION = "battery_fully_charged_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
