CyanogenMod/android_frameworks_base

SystemUI: handle hiding recents search bar dynamically
If the "Show recents search bar" is toggled inside Settings, we need to
catch that and reload the header bar as needed. Otherwise, having the
setting disabled would still include search bar bounds while the view
was hidden, causing some jarring animation jumps.

Ref: CYNGNOS-342

Change-Id: I74597d6d225af0395da6cb7a611bd230fc9e0675
Signed-off-by: Roman Birg <roman@cyngn.com>

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -46,6 +46,7 @@ import com.android.systemui.R;
 import com.android.systemui.RecentsComponent;
 import com.android.systemui.SystemUI;
 import com.android.systemui.SystemUIApplication;
+import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.recents.misc.Console;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.recents.model.RecentsTaskLoadPlan;
@@ -60,6 +61,8 @@ import com.android.systemui.recents.views.TaskViewTransform;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 import java.util.ArrayList;
+
+import cyanogenmod.providers.CMSettings;
 
 /**
  * Annotation for a method that is only called from the primary user's SystemUI process and will be
@@ -161,6 +164,35 @@ public class Recents extends SystemUI
         }
     }
 
+    class RecentsSettingsObserver extends UserContentObserver {
+
+        public RecentsSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void observe() {
+            super.observe();
+            mContext.getContentResolver().registerContentObserver(
+                    CMSettings.System.getUriFor(CMSettings.System.RECENTS_SHOW_SEARCH_BAR),
+                    false, this);
+            update();
+        }
+
+        @Override
+        protected void unobserve() {
+            super.unobserve();
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        protected void update() {
+            if (mConfig.updateShowSearch(mContext)) {
+                reloadHeaderBarLayout();
+            }
+        }
+    }
+
     static RecentsComponent.Callbacks sRecentsComponentCallbacks;
     static RecentsTaskLoadPlan sInstanceLoadPlan;
     static Recents sInstance;
@@ -171,6 +203,7 @@ public class Recents extends SystemUI
     TaskStackListenerImpl mTaskStackListener;
     RecentsOwnerEventProxyReceiver mProxyBroadcastReceiver;
     RecentsAppWidgetHost mAppWidgetHost;
+    RecentsSettingsObserver mSettingsObserver;
     boolean mBootCompleted;
     boolean mStartAnimationTriggered;
     boolean mCanReuseTaskStackViews = true;
@@ -258,6 +291,9 @@ public class Recents extends SystemUI
         TaskStackViewLayoutAlgorithm.initializeCurve();
         // Load the header bar layout
         reloadHeaderBarLayout();
+
+        mSettingsObserver = new RecentsSettingsObserver(mHandler);
+        mSettingsObserver.observe();
 
         // When we start, preload the data associated with the previous recent tasks.
         // We can use a new plan since the caches will be the same.
@@ -549,7 +585,8 @@ public class Recents extends SystemUI
         // Try and pre-emptively bind the search widget on startup to ensure that we
         // have the right thumbnail bounds to animate to.
         // Note: We have to reload the widget id before we get the task stack bounds below
-        if (mSystemServicesProxy.getOrBindSearchAppWidget(mContext, mAppWidgetHost) != null) {
+        if (mConfig.searchBarEnabled &&
+                mSystemServicesProxy.getOrBindSearchAppWidget(mContext, mAppWidgetHost) != null) {
             mConfig.getSearchBarBounds(mWindowRect.width(), mWindowRect.height(),
                     mStatusBarHeight, searchBarBounds);
         }

AospExtended/platform_frameworks_base-old

Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -46,6 +46,7 @@ import com.android.systemui.R;
 import com.android.systemui.RecentsComponent;
 import com.android.systemui.SystemUI;
 import com.android.systemui.SystemUIApplication;
+import com.android.systemui.turbo.UserContentObserver;
 import com.android.systemui.recents.misc.Console;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.recents.model.RecentsTaskLoadPlan;
@@ -161,6 +162,35 @@ public class Recents extends SystemUI
         }
     }
 
+    class RecentsSettingsObserver extends UserContentObserver {
+
+        public RecentsSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void observe() {
+            super.observe();
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.RECENTS_SHOW_SEARCH_BAR),
+                    false, this);
+            update();
+        }
+
+        @Override
+        protected void unobserve() {
+            super.unobserve();
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        protected void update() {
+            if (mConfig.updateShowSearch(mContext)) {
+                reloadHeaderBarLayout();
+            }
+        }
+    }
+
     static RecentsComponent.Callbacks sRecentsComponentCallbacks;
     static RecentsTaskLoadPlan sInstanceLoadPlan;
     static Recents sInstance;
@@ -171,6 +201,7 @@ public class Recents extends SystemUI
     TaskStackListenerImpl mTaskStackListener;
     RecentsOwnerEventProxyReceiver mProxyBroadcastReceiver;
     RecentsAppWidgetHost mAppWidgetHost;
+    RecentsSettingsObserver mSettingsObserver;
     boolean mBootCompleted;
     boolean mStartAnimationTriggered;
     boolean mCanReuseTaskStackViews = true;
@@ -258,6 +289,9 @@ public class Recents extends SystemUI
         TaskStackViewLayoutAlgorithm.initializeCurve();
         // Load the header bar layout
         reloadHeaderBarLayout();
+
+        mSettingsObserver = new RecentsSettingsObserver(mHandler);
+        mSettingsObserver.observe();
 
         // When we start, preload the data associated with the previous recent tasks.
         // We can use a new plan since the caches will be the same.
@@ -549,7 +583,8 @@ public class Recents extends SystemUI
         // Try and pre-emptively bind the search widget on startup to ensure that we
         // have the right thumbnail bounds to animate to.
         // Note: We have to reload the widget id before we get the task stack bounds below
-        if (mSystemServicesProxy.getOrBindSearchAppWidget(mContext, mAppWidgetHost) != null) {
+        if (mConfig.searchBarEnabled &&
+                mSystemServicesProxy.getOrBindSearchAppWidget(mContext, mAppWidgetHost) != null) {
             mConfig.getSearchBarBounds(mWindowRect.width(), mWindowRect.height(),
                     mStatusBarHeight, searchBarBounds);
         }